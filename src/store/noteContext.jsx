eb68fc1016c107fed89c85007c12487e5f05707e
Sadiquehmd <91723984+Sadiquehmd@users.noreply.github.com>
2024-09-04 13:21:22 +0530

    Update noteContext.jsx

--- src/store/noteContext.jsx
+++ src/store/noteContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useReducer, useEffect } from "react";
 
 
-const host = process.env.BASE_URL || "http://localhost:4000";
+const host = import.meta.env.BASE_URL || "http://localhost:4000";
 
 export const NoteContext = createContext({
   notes: [],
